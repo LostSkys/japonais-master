f53fc6106f6f0d658c992094b4b9899b72c8c156
clem <clement.ralli@gmail.com>
2026-04-28 17:07:34 +0200

    update permission android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'applangue',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https', // Force le schéma en https pour rassurer Android
+    allowNavigation: ['https://ton-site.netlify.app'] // Autorise ton lien Netlify
+  }
 };
 
 export default config;
